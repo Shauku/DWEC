5e5e99919b40980fc07cc78aab51ab8cbc1ba39a
cep <cep@A14-PC16-.cepnet1.net>
2013-11-29 12:29:35 +0100

    Capturas y toh

---
+++ CodeFriends/index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<html>
+    <head>
+        <?php include("./Plantillas/header.html"); ?>
+    </head>
+    <body>
+        <?php include("./Plantillas/cabecera.html"); ?>
+        
+        <?php include("./Plantillas/footer.html"); ?>
+    </body>
+</html>
+
