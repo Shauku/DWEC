2f2830ca8fabf2cac7f720c41661f8c16926598e
cep <cep@A14-PC16-.cepnet1.net>
2014-03-12 13:26:00 +0100

    mastermind

---
+++ MasterMind/js/master.js
@@ -0,0 +1,11 @@
+function cuantasOK(){
+    //TODO: Comprobar las fichas correctas
+}
+
+function cuantasKO(){
+    //TODO: Comprobar las fichas existentes pero no en su posicion
+}
+
+function generarCodigoOculto(){
+    //TODO: Genera la combinacion de fichas aleatoriamente
+}
